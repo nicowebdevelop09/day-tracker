f0430fb5b62279cd8c823c54c9bf9ba2dea2191a
nicowebdevelop09 <nico.webdevelop@gmail.com>
2026-08-07 11:27:12 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  // Percorso relativo: funziona sia su GitHub Pages (sottocartella)
-  // sia dentro l'app Capacitor, che serve i file da un percorso locale.
-  base: "./",
-  plugins: [react()],
-});
